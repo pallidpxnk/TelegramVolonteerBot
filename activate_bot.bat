dfbb6b496cb5e849407f51c8cfcabeaee84cb215
Mykola Holovanchuk <k.holovanchuk@icloud.com>
2022-09-01 01:14:58 +0200

    add start message and reply markup buttons

--- activate_bot.bat
+++ activate_bot.bat
@@ -2,7 +2,7 @@
 
 call %~dp0venv\Scripts\activate
 
-set TOKEN= your token
+set TOKEN=5321192794:AAG_mh2EWfY8fUpwDU4xHkD3hkNDKG2BzmA
 
 python main.py
 
--- handlers/client.py
+++ handlers/client.py
@@ -1,9 +1,13 @@
 from aiogram import types, Dispatcher
 from create_bot import bot
 
+from inline_buttons import obl
+
 
 async def command_start(message: types.Message):
-    await bot.send_message(message.from_user.id, 'start command')    # /start message
+    await bot.send_message(message.from_user.id, 'Вiтаю, ви почали роботу з ботом волонтером, який створенний для '
+                                                 'надання допомоги людям. Натиснiть або "Потрiбна допомога" для '
+                                                 'залишення заяви на допомогу', reply_markup=obl)
 
 
 async def command_help(message: types.Message):
@@ -12,4 +16,3 @@ async def command_help(message: types.Message):
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=['start'])
-    dp.register_message_handler(command_help, commands=['help'])
