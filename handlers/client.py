a9ccc4a86d4fb42f946b8e5623f38779e4781bc2
Mykola Holovanchuk <k.holovanchuk@icloud.com>
2022-09-03 22:15:13 +0200

    fix

--- handlers/client.py
+++ handlers/client.py
@@ -7,6 +7,9 @@ from inline_buttons import obl, category, agree_buttons, go_back_button, need_he
 
 
 class FSMClient(StatesGroup):
+    go_back = State()
+    start_state = State()
+    need_help = State()
     region_state = State()
     category_state = State()
     description_state = State()
@@ -15,35 +18,44 @@ class FSMClient(StatesGroup):
 
 
 async def command_start(message: types.Message):
+    await FSMClient.start_state.set()
     await bot.send_message(message.from_user.id, 'Вiтаю, ви почали роботу з ботом волонтером, який створенний для '
-                                                 'надання допомоги людям. Оберiть область в якiй потрiбна допомога:',
+                                                 'надання допомоги людям. Залиште заяву на допомогу',
                            reply_markup=need_help_button)
+    await FSMClient.next()
 
 
 async def need_help(message: types.Message):
     await bot.send_message(message.from_user.id, 'Оберiть область в якiй потрiбна допомога:', reply_markup=obl)
-
-
-async def go_back(message: types.Message):
-    await command_start(message)
+    await FSMClient.next()
 
 
 async def current_obl(query: types.CallbackQuery, state: FSMContext):
-    cur_obl = query.data
-    await FSMClient.region_state.set()
-    async with state.proxy() as data:
-        data['region_state'] = cur_obl
-    await FSMClient.next()
-    await bot.send_message(query.from_user.id, 'Оберiть категорiю:', reply_markup=category)
+    if query.data == 'back':
+        await FSMClient.start_state.set()
+        await bot.send_message(query.from_user.id, 'Вiтаю, ви почали роботу з ботом волонтером, який створенний для '
+                                                   'надання допомоги людям. Оберiть область в якiй потрiбна допомога:',
+                               reply_markup=need_help_button)
+        await FSMClient.next()
+    else:
+        async with state.proxy() as data:
+            data['region_state'] = query.data
+        await FSMClient.next()
+        await bot.send_message(query.from_user.id, 'Оберiть категорiю:', reply_markup=category)
 
 
 async def current_category(query: types.CallbackQuery, state: FSMContext):
-    cur_category = query.data
-    async with state.proxy() as data:
-        data['category_state'] = cur_category
-    await FSMClient.next()
-    await bot.send_message(query.from_user.id, 'Опишiть що саме вам потрiбно з цiєї категорiї:',
-                           reply_markup=go_back_button)
+    if query.data == 'back':
+        await FSMClient.start_state.set()
+        await bot.send_message(query.from_user.id, 'Вiтаю, ви почали роботу з ботом волонтером, який створенний для '
+                                                   'надання допомоги людям. Оберiть область в якiй потрiбна допомога:',
+                               reply_markup=need_help_button)
+        await FSMClient.next()
+    else:
+        async with state.proxy() as data:
+            data['category_state'] = query.data
+        await FSMClient.next()
+        await bot.send_message(query.from_user.id, 'Опишiть що саме вам потрiбно з цiєї категорiї:')
 
 
 async def input_description(message: types.Message, state: FSMContext):
@@ -54,34 +66,73 @@ async def input_description(message: types.Message, state: FSMContext):
 
 
 async def input_contact(query: types.CallbackQuery, state: FSMContext):
-    if query.data == 'yes':
-        async with state.proxy() as data:
-            data['contact_state'] = query.from_user.id
+    if query.data == 'back':
+        await FSMClient.start_state.set()
+        await bot.send_message(query.from_user.id, 'Вiтаю, ви почали роботу з ботом волонтером, який створенний для '
+                                                   'надання допомоги людям. Оберiть область в якiй потрiбна допомога:',
+                               reply_markup=need_help_button)
+        await FSMClient.next()
     else:
-        async with state.proxy() as data:
-            data['contact_state'] = ''
-    await FSMClient.next()
-    await bot.send_message(query.from_user.id, "Напишiть номер телефону для зв'язку з вами:",
-                           reply_markup=go_back_button)
+        if query.data == 'yes':
+            async with state.proxy() as data:
+                data['contact_state'] = query.from_user.id
+        else:
+            async with state.proxy() as data:
+                data['contact_state'] = ''
+        await FSMClient.next()
+        await bot.send_message(query.from_user.id, "Залишити номер телефону?",
+                               reply_markup=agree_buttons)
+
+
+async def enter_number(query: types.CallbackQuery, state: FSMContext):
+    if query.data == 'back':
+        await FSMClient.start_state.set()
+        await bot.send_message(query.from_user.id, 'Вiтаю, ви почали роботу з ботом волонтером, який створенний для '
+                                                   'надання допомоги людям. Оберiть область в якiй потрiбна допомога:',
+                               reply_markup=need_help_button)
+        await FSMClient.next()
+    else:
+        if query.data == 'yes':
+            await bot.send_message(query.from_user.id, "Напишiть ваш номер телефону для звя'зку:")
+        else:
+            async with state.proxy() as data:
+                data['number_state'] = ''
+            async with state.proxy() as data:
+                await bot.send_message(query.from_user.id, str(data))
+            await state.finish()
 
 
 async def input_number(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['number_state'] = message.text
-    async with state.proxy() as data:
-        await bot.send_message(message.from_user.id, str(data))
-    await state.finish()
+    if message.text.isnumeric():
+        async with state.proxy() as data:
+            data['number_state'] = message.text
+            await bot.send_message(message.from_user.id, str(data))
+        await state.finish()
+    else:
+        await bot.send_message(message.from_user.id, 'Номер введено неккоректно. Ввести номер ще раз?.',
+                               reply_markup=agree_buttons)
+        await FSMClient.number_state.set()
 
 
 def register_handlers_client(dp: Dispatcher):
-    dp.register_message_handler(command_start, commands=['start'])
-    dp.register_callback_query_handler(go_back, text='back')
-    dp.register_callback_query_handler(need_help, text='need_help')
+    dp.register_message_handler(command_start, commands=['start'], state=None)
+    dp.register_callback_query_handler(need_help, text='need_help', state=FSMClient.need_help)
+
     for i in range(len(list_obl)):
-        dp.register_callback_query_handler(current_obl, text=list_obl[i], state=None)
+        dp.register_callback_query_handler(current_obl, text=list_obl[i], state=FSMClient.region_state)
+    dp.register_callback_query_handler(current_obl, text='back', state=FSMClient.region_state)
+
     for i in range(len(list_category)):
         dp.register_callback_query_handler(current_category, text=list_category[i], state=FSMClient.category_state)
+    dp.register_callback_query_handler(current_category, text='back', state=FSMClient.category_state)
+
     dp.register_message_handler(input_description, state=FSMClient.description_state)
     dp.register_callback_query_handler(input_contact, text='yes', state=FSMClient.contact_state)
     dp.register_callback_query_handler(input_contact, text='no', state=FSMClient.contact_state)
+    dp.register_callback_query_handler(input_contact, text='back', state=FSMClient.contact_state)
+
+    dp.register_callback_query_handler(enter_number, text='yes', state=FSMClient.number_state)
+    dp.register_callback_query_handler(enter_number, text='no', state=FSMClient.number_state)
+    dp.register_callback_query_handler(enter_number, text='back', state=FSMClient.number_state)
+
     dp.register_message_handler(input_number, state=FSMClient.number_state)
